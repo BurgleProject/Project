e94da2df8460519c2906e996d02f14615a6ca8d9
embryobirthday <alexinverda@gmail.com>
2024-08-22 19:32:24 -0400

    fetch function works thank you abdullah

--- burgle-project/src/App.jsx
+++ burgle-project/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import BurgerPage from './pages/BurgerPage'
+import QuoteProvider from './context/QuotesProvider'
+
+
 
 function App() {
   console.log("HIIIIII")
   return (
     <div>
+      <QuoteProvider />
       <h1>hi</h1>
       <BurgerPage />
     </div>

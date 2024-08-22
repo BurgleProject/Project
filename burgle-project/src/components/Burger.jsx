2a109d4feb3b730697dd9970aac6b7ed2f4c7763
Marcy <madisontolentino@gmail.com>
2024-08-22 12:52:02 -0400

    hi

---
+++ burgle-project/src/components/Burger.jsx
@@ -0,0 +1,9 @@
+function BurgerButtons(){
+ return (
+ <div>
+   <button onClick={console.log('hi')}>Wake Up Routine!</button>
+ </div>
+)
+}
+
+export default BurgerButtons
---
+++ burgle-project/src/pages/Burgerpage.jsx
@@ -0,0 +1,11 @@
+import {BurgerButtons} from '../components/Burger.jsx'
+
+function BurgerPage(){
+ return(
+  <>
+   <BurgerButtons/>
+  </>
+ )
+}
+
+export default BurgerPage
